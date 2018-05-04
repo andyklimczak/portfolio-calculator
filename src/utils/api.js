6c4c5a52516888bd8ee38691eac424b8e36e3b48
Andy Klimczak <andyklimczak@fastmail.com>
2018-05-03 20:57:29 -0400

    remove param

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-async function getAPIData (period) {
+async function getAPIData () {
   const response = await axios.get('https://api.iextrading.com/1.0/stock/market/batch?symbols=voo,vb,vwo,vnq,shy,lqd,vea&types=stats')
   return response['data']
 }
